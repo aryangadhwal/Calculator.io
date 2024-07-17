d31ebc0fc16d9cee1530b3906241dd318d859c1b
Awalzz <146856647+aryangadhwal@users.noreply.github.com>
2024-07-17 21:29:44 +0530

    Main

---
+++ dist/index.js
@@ -0,0 +1,25 @@
+
+let calculator =  document.getElementById('outputScreen')
+function clr() {
+   calculator.innerText = '';
+}
+
+
+function del() {
+   calculator.innerText = calculator.innerText.slice(0, -1);
+}
+
+function display(n) {
+    calculator.innerText += n;
+}
+
+
+function calc() {
+    try {
+        calculator.innerText = eval(calculator.innerText);
+    } catch {
+        calculator.innerText = 'ERROR!';
+    }
+}
+
+
